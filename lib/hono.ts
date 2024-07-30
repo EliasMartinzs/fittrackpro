e671907bda5b50c79ac21e50baccaf145a0e1893
elias <rhyanlucaseliasmartins@gmail.com>
2024-07-30 17:14:52 -0300

    url api

--- lib/hono.ts
+++ lib/hono.ts
@@ -1,4 +1,6 @@
 import { AppType } from "@/app/api/[[...route]]/route";
 import { hc } from "hono/client";
 
-export const client = hc<AppType>("http://localhost:3000");
+const apiUrl = process.env.NEXT_PUBLIC_API_URL || "http://localhost:3000";
+
+export const client = hc<AppType>(apiUrl);
